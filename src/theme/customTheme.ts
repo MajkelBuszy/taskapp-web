05c3d60d6f2026d66171eabb2a43acc0cb1344fe
MajkelBuszy <swierzewski.biz@gmail.com>
2023-04-23 13:58:26 +0200

    project configuration

---
+++ src/theme/customTheme.ts
@@ -0,0 +1,16 @@
+import { createTheme, ThemeOptions } from '@mui/material';
+
+export const customTheme: ThemeOptions = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            light: '#757ce8',
+            main: '#3f50b5',
+            dark: '#002884',
+        },
+        background: {
+            default: '#000',
+            paper: '#151515',
+        }
+    }
+});
